5d797ad88062591a743d5708c45f106b858779ea
LONDON\coralb <coralb@london.edu>
2022-12-12 12:02:49 +0000

    Updated Contact.cs to reflect db table.

--- Phonebook/Contact.cs
+++ Phonebook/Contact.cs
@@ -1,9 +1,13 @@
-﻿namespace Phonebook
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phonebook
 {
     public class Contact
     {
+        [Key]
+        public int ContactId { get; set; }
+        
         public string Name { get; set; }
 
-        public List<string> Phones { get; set; }
     }
 }
