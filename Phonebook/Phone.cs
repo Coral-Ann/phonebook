dc11860e6c4ac2039f2a3e957d2e3dea63129755
LONDON\coralb <coralb@london.edu>
2022-12-12 13:36:08 +0000

    Added Phone.cs.

---
+++ Phonebook/Phone.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Phonebook
+{
+    public class Phone
+    {
+        [Key]
+        public int PhoneId { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        [ForeignKey(nameof(Contact.ContactId))]
+        public int ContactId { get; set; }
+
+    }
+}
